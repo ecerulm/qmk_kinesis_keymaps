7271e2884c905b4e1bea5df8be188fda0bff7413
Ruben Laguna Macias <ruben.laguna@gmail.com>
2023-03-13 17:27:25 +0100

    update to qmk 0.20.1

--- ecerulm/keymap.c
+++ ecerulm/keymap.c
@@ -63,11 +63,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
            KC_TAB, KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,
            KC_LCTL,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,
            KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,
-                   KC_GRV ,KC_LEAD,KC_LEFT,KC_RGHT,
+                   KC_GRV ,QK_LEAD,KC_LEFT,KC_RGHT,
 			                      KC_LGUI,KC_LALT,
                                           KC_LCTL,
                            KC_BSPC,KC_DEL,KC_HYPR,
-    KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,KC_PSCR ,KC_SLCK  ,KC_PAUS, RESET, RESET,
+    KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,KC_PSCR ,KC_SCRL ,KC_PAUS, QK_REBOOT, QK_BOOT,
 	KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,KC_MINS,
 	KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,KC_BSLS,
 	KC_H   ,KC_J   ,KC_K   ,KC_L   ,KC_SCLN,KC_QUOT,
@@ -79,4 +79,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
-LEADER_EXTERNS();
